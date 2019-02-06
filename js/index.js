2287c1e91ef6e3a1423653bfe5a1f83110884d0b
Abdullah Farooq <abdullah.gb@hotmail.com>
2019-02-07 06:23:35 +0700

    Initlize project

---
+++ js/index.js
@@ -0,0 +1,38 @@
+(function () {
+  $(function() {
+    $(".login--container").removeClass("preload");
+    this.timer = window.setTimeout((function(_this) {
+      return function() {
+        return $(".login--container").toggleClass("login--active");
+      };
+    })(this), 2000);
+    return $(".js-toggle-login").click((function(_this) {
+      return function() {
+        window.clearTimeout(_this.timer);
+        $(".login--container").toggleClass("login--active");
+        return $(".login--username-container input").focus();
+      };
+    })(this));
+  });
+
+}).call(this);
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
